741163e4911020e31126b10b8e5564a48fed3b60
brentonduvall <brenton.duvall.18@cnu.edu>
2021-04-19 11:08:59 -0400

    Substantial Progress

--- Smalltalk.cpp
+++ Smalltalk.cpp
@@ -8,6 +8,7 @@
 #include "includes/Smalltalk.h"
 #include "includes/constants.h"
 #include "includes/Watch.h"
+#include <memory>
 
 using namespace std;
 
@@ -22,23 +23,23 @@ Smalltalk::~Smalltalk(void) {
 }
 
 std::string Smalltalk::saySomething() {
+	string person =  iPerson + " ";
 	if (current_phrase == 0) {
 		current_phrase++;
-		string one =  iPerson + " ";
 
 		mySmallTalk[current_phrase%2];
-		nationality;
 
-		return cout << nationality << "" << one << ": " << mySmallTalk[current_phrase%2]<< endl;
+		return  nationality + "" + person + ": " + mySmallTalk[current_phrase%2];
 	}
 
-	if (current_phrase == mySmallTalk.size()) {
+	if ((unsigned)current_phrase == mySmallTalk.size()) {
 		current_phrase = 0;
 	}
 	//current_phrase++;
 	//iPerson++;
 
-	return nationality + " " + this->iPerson + ": " + mySmallTalk[current_phrase%2];
+
+	return  nationality + "" + person + ": " + mySmallTalk[current_phrase%2];
 }
 
 std::string Smalltalk::getTime() {
@@ -50,3 +51,29 @@ std::string Smalltalk::getTime() {
 		return I_DO_NOT_HAVE_A_WATCH;
 	}
 }
+
+std::unique_ptr<Watch>  Smalltalk::takeWatch() {
+	if (pWatch != 0) {
+		pWatch.reset();
+	}
+	return std::unique_ptr<Watch>(nullptr);
+
+}
+
+bool Smalltalk::giveWatch(std::unique_ptr<Watch> &pWatch) {
+	if (pWatch == NULL) {
+		return false;
+	}
+
+	if (pWatch) {
+		return false;
+	}
+
+	this->pWatch = std::move(pWatch);
+	return true;
+}
+
+void populatePhrases() {
+
+}
+
--- Smalltalk_American.cpp
+++ Smalltalk_American.cpp
@@ -5,6 +5,29 @@
  *      Author: guest-l9gf4s
  */
 
+#include "./includes/Smalltalk_American.h"
+#include "./includes/Smalltalk.h"
+#include "./includes/constants.h"
 
+Smalltalk_American::Smalltalk_American(int iPerson) : Smalltalk(AMERICAN,iPerson) {
+}
 
+Smalltalk_American::Smalltalk_American(std::string myNationality,int iPerson) : Smalltalk(myNationality,iPerson) {}
 
+//: nationality(AMERICAN), iPerson(iPerson) {}
+
+//Smalltalk_Brit::populatePhrases()  {
+	//mySmalltalk = new vecto
+//}
+
+Smalltalk_American::~Smalltalk_American(void) {
+
+}
+
+void Smalltalk_American::populatePhrases() {
+	Smalltalk::mySmallTalk.push_back(AMERICAN_PHRASE_1);
+	Smalltalk::mySmallTalk.push_back(AMERICAN_PHRASE_2),
+	Smalltalk::mySmallTalk.push_back(AMERICAN_PHRASE_3),
+	Smalltalk::mySmallTalk.push_back(AMERICAN_PHRASE_4),
+	Smalltalk::mySmallTalk.push_back(AMERICAN_PHRASE_5);
+}
